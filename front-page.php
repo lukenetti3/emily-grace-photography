b0df06b0d153e7615a4896e4218145ac4996b167
Luke Netti <luke.netti3@gmail.com>
2019-05-02 22:04:25 -0400

    Working on testimonial section

--- front-page.php
+++ front-page.php
@@ -1,10 +1,18 @@
 <?php get_header() ?>
 
-<!-- Grab Images -->
+<!-- Grab Content -->
 <?php
 $image_left = get_field('image_left');
 $image_center = get_field('image_center');
 $image_right = get_field('image_right');
+
+$heading1 = get_field('heading_1');
+$heading2 = get_field('heading_2');
+$paragraph1 = get_field('paragraph_1');
+$paragraph2 = get_field('paragraph_2');
+
+$testimonialImage = get_field('testimonial_image');
+$testimonialText = get_field('testimonial_text');
 ?>
 
 
@@ -28,30 +36,36 @@ $image_right = get_field('image_right');
   <div class="container-fluid py-5">
     <div class="row align-items-center">
       <div class="col-12 col-xl-6 bg-beige p-5-xl text-center">
-        <hr class="remove"><h2 class="d-inline px-5">Bride?</h2><hr>
-        <p class="lead py-4 p-line-height">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-           sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-           aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-           duo dolores et
-           nonumy eirmod tempor
+        <hr class="remove"><h2 class="d-inline px-5"><?php echo $heading1 ?></h2><hr>
+        <p class="lead py-4 p-line-height">
+          <?php echo $paragraph1 ?>
          </p>
          <button class="btn btn-outline-primary">
            <a href="#">Click Here</a>
          </button>
       </div>
       <div class="col-12 col-xl-6 bg-grey p-5-xl text-center">
-        <hr class="remove"><h2 class="d-inline px-5">Family?</h2><hr>
-        <p class="lead py-4 p-line-height">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-           sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-           aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-           duo dolores et
-           nonumy eirmod tempor
+        <hr class="remove"><h2 class="d-inline px-5"><?php echo $heading2 ?></h2><hr>
+        <p class="lead py-4 p-line-height">
+          <?php echo $paragraph2 ?>
          </p>
          <button class="btn btn-outline-primary">
            <a href="#">Click Here</a>
          </button>
       </div>
+    </div>
+  </div>
+</section>
 
+<section id="section-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <img class="img-fluid w-100 h-100" src="<?php echo $testimonialImage ?>" alt="testimonial image">
+      </div>
+      <div class="col-12 col-md-6">
+        <span class="offset-block"></span>
+      </div>
     </div>
   </div>
 </section>
