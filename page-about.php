b042330ecbd70682b00dc5a9ba1ec75e8f7e480c
Luke Netti <luke.netti3@gmail.com>
2019-05-19 19:43:32 -0400

    Work on about page

--- page-about.php
+++ page-about.php
@@ -11,16 +11,27 @@ $aboutTxt = get_field('text');
     <div class="row">
       <div class="col-12 col-lg-6 order-lg-2">
         <h2>About Me</h2>
+        <div class="row">
+          <div class="col-12">
+            <p class="lead p-line-height"><?php echo $aboutTxt ?></p>
+          </div>
+        </div>
       </div>
       <div class="col-12 col-lg-6 order-lg-1">
+        <div class="overlay-about"></div>
         <img class="img-fluid w-100 h-100" src="<?php echo $aboutImage ?>" alt="Image of Emily">
-      </div>
-      <div class="col-12 col-lg-6 order-lg-2">
-        <p class="lead p-line-height"><?php echo $aboutTxt ?></p>
-      </div>
       </div>
     </div>
   </div>
 </section>
+
+<section id="about-section3">
+  <div class="container">
+    <hr>
+  </div>
+</section>
+
+
+
 
 <?php get_footer() ?>
