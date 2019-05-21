09ba46fd01ba30c4ab2ccee3fa2c2440943e2681
Luke Netti <luke.netti3@gmail.com>
2019-05-20 20:34:08 -0400

    About Page: done

--- page-about.php
+++ page-about.php
@@ -4,9 +4,12 @@
 <?php
 $aboutImage = get_field('image');
 $aboutTxt = get_field('text');
+
+$journeyPL = get_field('paragraph_left');
+$journeyPR = get_field('paragraph_right');
 ?>
 
-<section id="about-section1">
+<section id="about-section1" class="section-1">
   <div class="container">
     <div class="row">
       <div class="col-12 col-lg-6 order-lg-2">
@@ -19,19 +22,26 @@ $aboutTxt = get_field('text');
       </div>
       <div class="col-12 col-lg-6 order-lg-1">
         <div class="overlay-about"></div>
-        <img class="img-fluid w-100 h-100" src="<?php echo $aboutImage ?>" alt="Image of Emily">
+        <img class="img-fluid w-75 h-85" src="<?php echo $aboutImage ?>" alt="Image of Emily">
       </div>
     </div>
   </div>
 </section>
 
-<section id="about-section3">
-  <div class="container">
+<section id="about-section2">
+  <div class="container py-4">
     <hr>
+    <h2 class="text-center p-5">My Journey</h2>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <p class="lead p-line-height px-3"><?php echo $journeyPL ?></p>
+      </div>
+      <div class="col-12 col-md-6">
+        <p class="lead p-line-height px-3"><?php echo $journeyPR ?></p>
+      </div>
+    </div>
   </div>
 </section>
-
-
 
 
 <?php get_footer() ?>
