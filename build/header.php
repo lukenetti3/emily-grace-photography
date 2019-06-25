efbca4e955b48b8a708f9ce355f18f30514352f4
Luke Netti <luke.netti3@gmail.com>
2019-06-24 20:57:16 -0400

    Worked on logo and header files

--- build/header.php
+++ build/header.php
@@ -25,31 +25,42 @@
 
 	<header id="masthead" class="site-header">
 
-		<nav class="navbar navbar-expand-lg navbar-light fixed-top py-4">
-			<div class="container nav-area d-flex flex-column">
-				<!-- <a class="navbar-brand" id="site-title" href="<?php echo get_bloginfo('wpurl') ?>"><?php echo get_bloginfo() ?></a> -->
-				<a href="<?php echo get_bloginfo('wpurl') ?>">
-					<!-- <p class="testing text-center">EMILY NETTI</p>
-					<p class="testing-2 text-center">photography</p> -->
-						<img class="logo" src="<?php echo get_bloginfo('wpurl')?>/wp-content/themes/emily-gphotography/source/images/logo.png" alt="Emily Netti Logo">
+		<nav class="navbar navbar-expand-lg navbar-light fixed-top py-5">
+			<div class="container nav-area d-flex flex-row">
+					
+				<a href="<?php echo get_bloginfo('wpurl') ?>" class="d-block d-sm-none">
+					<img class="logo ml-4" src="<?php echo get_bloginfo('wpurl')?>/wp-content/themes/emily-gphotography/source/images/logo.png" alt="Emily Netti Logo">
 				</a>
-			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			    <span class="navbar-toggler-icon"></span>
-			  </button>
-			  <div class="collapse navbar-collapse pb-3" id="navbarNav">
-					<?php
-					wp_nav_menu( array(
-						'menu' 					 => 'header',
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-						'menu_class'     => 'navbar-nav',
-					) );
-					?>
-			  </div>
+
+				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				    <span class="navbar-toggler-icon"></span>
+				  </button>
+				  <div class="collapse navbar-collapse pb-sm-3 justify-content-center" id="navbarNav">
+						<?php
+						wp_nav_menu( array(
+							'menu' 					 => 'header-left',
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'menu_class'     => 'navbar-nav',
+						) );
+						?>
+				  </div>
+
+						<a href="<?php echo get_bloginfo('wpurl') ?>" class="d-none d-sm-block">
+								<img class="logo" src="<?php echo get_bloginfo('wpurl')?>/wp-content/themes/emily-gphotography/source/images/logo.png" alt="Emily Netti Logo">
+						</a>
+
+					<div class="collapse navbar-collapse pb-1 justify-content-center" id="navbarNav">
+						<?php
+						wp_nav_menu( array(
+							'menu' 					 => 'header-right',
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'menu_class'     => 'navbar-nav',
+						) );
+						?>
+				  </div>
 			</div>
 		</nav>
-
-
 	</header><!-- #masthead -->
-
 	<div id="content" class="site-content">
